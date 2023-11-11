959cd890054fc1cc326d36cfb503db0f3c3468d8
micaelnisa <mikassantosnisa@gmail.com>
2023-11-11 13:56:08 +0000

    disposição e botoes

---
+++ Meta 2/meta2.js
@@ -0,0 +1,95 @@
+let botaoCirculos = [];
+let canvas;
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 50, 2, 0, PI);
+  botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 6, 50, 2, PI, 0);
+  botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 50, 2, -PI, 0);
+  botaoCirculos[4] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight - windowHeight / 6, 50, 2, 0, -PI);
+}
+
+function draw() {
+  background(50);
+  for (let i = 1; i < botaoCirculos.length; i++) {
+    botaoCirculos[i].display();
+  }
+}
+
+function touchStarted() {
+  for (let j = 0; j < touches.length; j++) {
+    let x = touches[j].x;
+    let y = touches[j].y;
+
+    for (let i = 1; i < botaoCirculos.length; i++) {
+      botaoCirculos[i].verificarToque(x, y);
+    }
+  }
+}
+
+class BotaoCirculos {
+  constructor(x, y, diametroCentral, numCirculosSatelites, angSeletoresInicio, angSeletoresFim) {
+    this.x = x;
+    this.y = y;
+    this.angSeletoresInicio = angSeletoresInicio;
+    this.angSeletoresFim = angSeletoresFim;
+    this.diametroCentral = diametroCentral;
+    this.numCirculosSatelites = numCirculosSatelites;
+    this.circulosSatelites = [];
+
+    for (let i = 0; i < numCirculosSatelites; i++) {
+      let angulo = map(i, 0, numCirculosSatelites, angSeletoresInicio, angSeletoresFim);
+      let raio = this.diametroCentral * 1.3;
+      let sateliteX = this.x + raio * cos(angulo);
+      let sateliteY = this.y + raio * sin(angulo);
+      this.circulosSatelites.push(new SeletorCirculos(sateliteX, sateliteY, 25, i + 1));
+    }
+  }
+
+  display() {
+    fill(0, 128, 255);
+    ellipse(this.x, this.y, this.diametroCentral, this.diametroCentral);
+
+    for (let i = 0; i < this.numCirculosSatelites; i++) {
+      this.circulosSatelites[i].display();
+    }
+  }
+
+  verificarToque(px, py) {
+    for (let i = 0; i < this.numCirculosSatelites; i++) {
+      if (this.circulosSatelites[i].contains(px, py)) {
+        this.circulosSatelites[i].mudarCor();
+      }
+    }
+  }
+}
+
+class SeletorCirculos {
+  constructor(x, y, diametro, numero) {
+    this.x = x;
+    this.y = y;
+    this.diametro = diametro;
+    this.numero = numero;
+    this.cor = color(255);
+  }
+
+  display() {
+    fill(this.cor);
+    ellipse(this.x, this.y, this.diametro, this.diametro);
+
+    fill(0);
+    textAlign(CENTER, CENTER);
+    textSize(12);
+    text(this.numero, this.x, this.y);
+  }
+
+  contains(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    return d < this.diametro / 2;
+  }
+
+  mudarCor() {
+    this.cor = color(255, 250, 0);
+  }
+}
