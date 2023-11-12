e7b6767b228f66f7c14c50ac95be46a64e27efab
tomasoliveiraf <tomasten9@gmail.com>
2023-11-12 00:22:15 +0000

    toma

--- Meta 2/meta2.js
+++ Meta 2/meta2.js
@@ -2,18 +2,63 @@ let botaoCirculos = [];
 let canvas;
 
 //SONS
+<<<<<<< Updated upstream
 let melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5;
 
 //PRELOAD
 function preload() {
   soundFormats('mp3');
   melodia1 = loadSound("../som/Melodia_1.mp3")
+=======
+var melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5;
+var slider;
+
+function preload() {
+    //SOM
+
+    //MELODIAS
+    console.log("Carregando Melodia_1.mp3");
+    melodia1 = loadSound("som/Melodia_1.mp3");
+    melodia2 = loadSound("som/Melodia_2.mp3");
+    melodia3 = loadSound("som/Melodia_3.mp3");
+    melodia4 = loadSound("som/Melodia_4.mp3");
+    melodia5 = loadSound("som/Melodia_5.mp3");
+
+    //BAIXOS
+    baixo1 = loadSound("som/Baixo_1.mp3");
+    baixo2 = loadSound("som/Baixo_2.mp3");
+    baixo3 = loadSound("som/Baixo_3.mp3");
+    baixo4 = loadSound("som/Baixo_4.mp3");
+    baixo5 = loadSound("som/Baixo_5.mp3");
+
+    //PERCUSSÕES
+    perc1 = loadSound("som/Perc_1.mp3");
+    perc2 = loadSound("som/Perc_2.mp3");
+    perc3 = loadSound("som/Perc_3.mp3");
+    perc4 = loadSound("som/Perc_4.mp3");
+    perc5 = loadSound("som/Perc_5.mp3");
+
+    //APITOS
+    apito1 = loadSound("som/Apito_1.mp3");
+    apito2 = loadSound("som/Apito_2.mp3");
+    apito3 = loadSound("som/Apito_3.mp3");
+    apito4 = loadSound("som/Apito_4.mp3");
+    apito5 = loadSound("som/Apito_5.mp3");
+>>>>>>> Stashed changes
 }
 
 function setup() {
+
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
+<<<<<<< Updated upstream
   melodia1.play();
+=======
+
+  //SOM
+  slider = createSlider(0, 1, 0.5, 0.01);
+
+>>>>>>> Stashed changes
   botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 50, 2, 0, PI);
   botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 6, 50, 2, PI, 0);
   botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 50, 2, -PI, 0);
@@ -21,7 +66,9 @@ function setup() {
 }
 
 function draw() {
-  background(50);
+  melodia1.setVolume(slider.value());
+
+  /*background(50);*/
   for (let i = 1; i < botaoCirculos.length; i++) {
     botaoCirculos[i].display();
   }
@@ -36,6 +83,12 @@ function touchStarted() {
       botaoCirculos[i].verificarToque(x, y);
     }
   }
+}
+
+function mousePressed() {
+  melodia1.play();
+  perc1.play();
+  baixo1.play();
 }
 
 class BotaoCirculos {
