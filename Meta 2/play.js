2b5240956fe0bff047aca8c06e7a0bd441ba30e1
tomasoliveiraf <tomasten9@gmail.com>
2023-11-14 00:53:54 +0000

    sons a funcionar individualmente

--- Meta 2/play.js
+++ Meta 2/play.js
@@ -2,6 +2,80 @@ let playButton;
 let gameState = "play"; // estado do jogo para podermos ir alterando conforme o que esta a acontecer
 let botaoCirculos = [];
 let canvas;
+<<<<<<< Updated upstream
+=======
+let corMelodia, corPercursão, corApitos, corGraves;
+
+// SONS
+let playButton;
+let melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5;
+let allSounds = [];
+let playbackSpeedSlider;
+
+let selectedCategory = null;
+
+function preload() {
+  // MELODIAS
+  melodia1 = loadSound("som/Melodia_1.mp3");
+  melodia2 = loadSound("som/Melodia_2.mp3");
+  melodia3 = loadSound("som/Melodia_3.mp3");
+  melodia4 = loadSound("som/Melodia_4.mp3");
+  melodia5 = loadSound("som/Melodia_5.mp3");
+
+  melodia1.category = "melodia";
+  melodia2.category = "melodia";
+  melodia3.category = "melodia";
+  melodia4.category = "melodia";
+  melodia5.category = "melodia";
+
+  // BAIXOS
+  baixo1 = loadSound("som/Baixo_1.mp3");
+  baixo2 = loadSound("som/Baixo_2.mp3");
+  baixo3 = loadSound("som/Baixo_3.mp3");
+  baixo4 = loadSound("som/Baixo_4.mp3");
+  baixo5 = loadSound("som/Baixo_5.mp3");
+
+  baixo1.category = "baixo";
+  baixo2.category = "baixo";
+  baixo3.category = "baixo";
+  baixo4.category = "baixo";
+  baixo5.category = "baixo";
+
+  // PERCUSSÕES
+  perc1 = loadSound("som/Perc_1.mp3");
+  perc2 = loadSound("som/Perc_2.mp3");
+  perc3 = loadSound("som/Perc_3.mp3");
+  perc4 = loadSound("som/Perc_4.mp3");
+  perc5 = loadSound("som/Perc_5.mp3");
+
+  perc1.category = "perc";
+  perc2.category = "perc";
+  perc3.category = "perc";
+  perc4.category = "perc";
+  perc5.category = "perc";
+
+  // APITOS
+  apito1 = loadSound("som/Apito_1.mp3");
+  apito2 = loadSound("som/Apito_2.mp3");
+  apito3 = loadSound("som/Apito_3.mp3");
+  apito4 = loadSound("som/Apito_4.mp3");
+  apito5 = loadSound("som/Apito_5.mp3");
+
+  apito1.category = "apitos";
+  apito2.category = "apitos";
+  apito3.category = "apitos";
+  apito4.category = "apitos";
+  apito5.category = "apitos";
+
+  // Add all sounds to the array
+  allSounds = [melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5];
+
+  // Set volume to 0 for all sounds
+  allSounds.forEach(sound => {
+    sound.setVolume(0);
+  });
+}
+>>>>>>> Stashed changes
 
 function setup() {
  canvas = createCanvas(windowWidth, windowHeight);
@@ -20,11 +94,77 @@ function setup() {
   sair = new BotaoRedondo( 100, 100, 50, color (255, 0, 0));
   jogar = new BotaoRedondo( 100, 200, 50, color (0, 255, 0));
 
+<<<<<<< Updated upstream
   /* -------------------------------------- Codigo para gamestate jogar ----------------------------------------*/     
   botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 50, 2, 0, PI);
   botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 6, 50, 2, PI, 0);
   botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 50, 2, -PI, 0);
   botaoCirculos[4] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight - windowHeight / 6, 50, 2, 0, -PI);
+=======
+  sair = new BotaoRedondo(100, 100, 50, color(255, 0, 0));
+  jogar = new BotaoRedondo(100, 200, 50, color(0, 255, 0));
+
+  botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 100, 2, 0, PI, corMelodia);
+  botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 6, 100, 2, PI, 0, corApitos);
+  botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 2, -PI, 0, corGraves);
+  botaoCirculos[4] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 2, 0, -PI, corPercursão);
+
+  playButton = createButton('Play');
+  playButton.position(windowWidth / 2, windowHeight / 2);
+  playButton.mousePressed(play);
+
+  playbackSpeedSlider = createSlider(0.5, 1.5, 1, 0.1);
+  playbackSpeedSlider.position(windowWidth / 2, (windowHeight / 2 - 100));
+  playbackSpeedSlider.style('width', '80px');
+  playbackSpeedSlider.hide();
+}
+
+function play() {
+  // Play all sounds simultaneously with volume 0
+  allSounds.forEach(sound => {
+    sound.play();
+    sound.setVolume(0);
+  });
+}
+
+function toggleVolume(sound, category) {
+  if (selectedCategory === category) {
+    // Se a categoria já estiver selecionada, desligue todos os sons dessa categoria
+    allSounds.filter(s => s !== sound && s.category === category).forEach(s => s.setVolume(0));
+  } else {
+    // Se a categoria não estiver selecionada, desligue todos os sons de outras categorias
+    allSounds.filter(s => s.category !== category).forEach(s => s.setVolume(0));
+  }
+
+  // Alterna o volume do som específico
+  sound.setVolume(sound.getVolume() === 0 ? 1 : 0);
+
+  // Atualiza a categoria selecionada
+  selectedCategory = sound.getVolume() === 0 ? null : category;
+}
+
+function createToggleButton(label, sound, index) {
+  let button = createButton(label);
+
+  if (index <= 5) {
+    button.position(windowWidth / 5, 150 + index * 30);
+  } else if (index > 5 && index <= 10) {
+    button.position(windowWidth - windowWidth / 5, 10 + index * 30);
+  } else if (index > 10 && index <= 15) {
+    button.position(windowWidth - windowWidth / 5, windowHeight - (-150 + index * 30));
+  } else if (index > 15 && index <= 20) {
+    button.position(windowWidth / 5, windowHeight - (-300 + index * 30));
+  }
+  button.mousePressed(() => toggleVolume(sound));
+  button.mouseClicked(() => playApito(index));
+>>>>>>> Stashed changes
+}
+
+function playApito(apitoNumber, category) {
+  // Garante que apenas o som da categoria selecionada seja reproduzido
+  if (selectedCategory === category) {
+    allSounds[apitoNumber - 1].play();
+  }
 }
 
 function draw() {
@@ -51,6 +191,17 @@ function draw() {
 
 
 
+<<<<<<< Updated upstream
+=======
+function play() {
+  // Play all sounds
+  allSounds.forEach(sound => {
+      sound.play();
+  });
+}
+
+
+>>>>>>> Stashed changes
 function touchStarted() {
 
 
@@ -173,7 +324,6 @@ class BotaoRedondo {
     }
   }
 }
-
 
 
 class BotaoCirculos {
