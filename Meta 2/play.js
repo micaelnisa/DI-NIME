b9d4ef5e2185b2a01e150a648a73673755ecd47a
inesmg0 <inesmendes022@gmail.com>
2023-12-09 17:48:09 +0000

    ai tãn lindo

--- Meta 2/play.js
+++ Meta 2/play.js
@@ -1,59 +1,80 @@
-let inicioButton;
-let gameState = "play";
-let botaoCirculos = [];
+let gamestate = "inicio";
 let canvas;
-let corMelodia, corPercursão, corApitos, corGraves;
+let corMelodia, corPercussao, corApitos, corGraves;
 
+//gamestate inicio
+let inicioButton, iniciosvg;
+//gamestate elementos
+let botaoMelodia, botaoPercussao, botaoApitos, botaoGraves, melodiasvg, percussaosvg, apitossvg, gravessvg;
+let indexText = 0;
+let instrucaoElementos = ['Pressione o elemento que pretende controlar.', 'Cada jogador deverá escolher entre 1 e 2 elementos.'];
 
+//gamestate jogar
+//sons
 let time = 0;
 let frameRate = 30;
-
-// SONS
 let playButton;
-let melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5;
 let allSounds = [];
-let playbackSpeedSlider;
+let melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5;
+//painel
+let fundogeral, fundogeralsvg;
+let speedSlider;
+//botoes
+let botaoCirculos = [];
 
-function preload() {
-  // MELODIAS
-  melodia1 = loadSound("som/Melodia_1.mp3");
-  melodia2 = loadSound("som/Melodia_2.mp3");
-  melodia3 = loadSound("som/Melodia_3.mp3");
-  melodia4 = loadSound("som/Melodia_4.mp3");
-  melodia5 = loadSound("som/Melodia_5.mp3");
 
-  // BAIXOS
-  baixo1 = loadSound("som/Baixo_1.mp3");
-  baixo2 = loadSound("som/Baixo_2.mp3");
-  baixo3 = loadSound("som/Baixo_3.mp3");
-  baixo4 = loadSound("som/Baixo_4.mp3");
-  baixo5 = loadSound("som/Baixo_5.mp3");
+function preload(){
+  //gamestate inicio
+  iniciosvg = loadImage('svg/inicio.svg');
 
-  // PERCUSSÕES
-  perc1 = loadSound("som/Perc_1.mp3");
-  perc2 = loadSound("som/Perc_2.mp3");
-  perc3 = loadSound("som/Perc_3.mp3");
-  perc4 = loadSound("som/Perc_4.mp3");
-  perc5 = loadSound("som/Perc_5.mp3");
+  //gamestate elementos
+  melodiasvg = loadImage('svg/botaoMelodia.svg');
+  percussaosvg = loadImage('svg/botaoPercussao.svg');
+  apitossvg = loadImage('svg/botaoApitos.svg');
+  gravessvg = loadImage('svg/botaoGraves.svg');
 
-  // APITOS
-  apito1 = loadSound("som/Apito_1.mp3");
-  apito2 = loadSound("som/Apito_2.mp3");
-  apito3 = loadSound("som/Apito_3.mp3");
-  apito4 = loadSound("som/Apito_4.mp3");
-  apito5 = loadSound("som/Apito_5.mp3");
+  //gamestate jogar
+  fundogeralsvg = loadImage('svg/fundopainelgeral.svg');
 
-  // Add all sounds to the array
+    // MELODIAS
+    melodia1 = loadSound("som/Melodia_1.mp3");
+    melodia2 = loadSound("som/Melodia_2.mp3");
+    melodia3 = loadSound("som/Melodia_3.mp3");
+    melodia4 = loadSound("som/Melodia_4.mp3");
+    melodia5 = loadSound("som/Melodia_5.mp3");
+  
+    // BAIXOS
+    baixo1 = loadSound("som/Baixo_1.mp3");
+    baixo2 = loadSound("som/Baixo_2.mp3");
+    baixo3 = loadSound("som/Baixo_3.mp3");
+    baixo4 = loadSound("som/Baixo_4.mp3");
+    baixo5 = loadSound("som/Baixo_5.mp3");
+  
+    // PERCUSSÕES
+    perc1 = loadSound("som/Perc_1.mp3");
+    perc2 = loadSound("som/Perc_2.mp3");
+    perc3 = loadSound("som/Perc_3.mp3");
+    perc4 = loadSound("som/Perc_4.mp3");
+    perc5 = loadSound("som/Perc_5.mp3");
+  
+    // APITOS
+    apito1 = loadSound("som/Apito_1.mp3");
+    apito2 = loadSound("som/Apito_2.mp3");
+    apito3 = loadSound("som/Apito_3.mp3");
+    apito4 = loadSound("som/Apito_4.mp3");
+    apito5 = loadSound("som/Apito_5.mp3");
+  
+  // Add sons ao array
   allSounds = [melodia1, melodia2, melodia3, melodia4, melodia5, baixo1, baixo2, baixo3, baixo4, baixo5, perc1, perc2, perc3, perc4, perc5, apito1, apito2, apito3, apito4, apito5];
   melodias = [melodia1, melodia2, melodia3, melodia4, melodia5];
   baixos = [baixo1, baixo2, baixo3, baixo4, baixo5];
   percussoes = [perc1, perc2, perc3, perc4, perc5];
   apitos = [apito1, apito2, apito3, apito4, apito5];
 
-  // Set volume to 0 for all sounds
-  allSounds.forEach(sound => {
-    sound.setVolume(0);
-  });
+    // Volume inicial igual a 0 (sons todos a dar em mute)
+    allSounds.forEach(sound => {
+      sound.setVolume(0);
+    });
 }
 
 function setup() {
@@ -61,52 +82,119 @@ function setup() {
   canvas.position(0, 0);
 
   corMelodia = color(32, 181, 115);
-  corPercursão = color(0, 169, 157);
+  corPercussao = color(0, 169, 157);
   corApitos = color(140, 198, 63);
   corGraves = color(217, 224, 33);
 
-  inicioButton = new BotaoRedondo(width / 2, height / 2, 200, color(0, 113, 188));
+  //gamestate inicio
+  inicioButton = new Botao (width/2, height/2, iniciosvg, width/5);
 
-  melodia_button = new BotaoRedondo((width / 2) - 100, (height / 2) - 100, 100, corMelodia);
-  percursao_button = new BotaoRedondo((width / 2) - 100, (height / 2) + 100, 100, corPercursão);
-  apitos_button = new BotaoRedondo((width / 2) + 100, (height / 2) - 100, 100, corApitos);
-  graves_button = new BotaoRedondo((width / 2) + 100, (height / 2) + 100, 100, corGraves);
+  //gamestate elementos (to a usar width para definir diametro e posicao para ser kinda responsivo)
+  botaoMelodia = new Botao (width/2 - (width/8), height/2 - (width/8), melodiasvg, width/7);
+  botaoPercussao = new Botao (width/2 + (width/8), height/2 - (width/8), percussaosvg, width/7);
+  botaoApitos = new Botao (width/2 - (width/8), height/2 + (width/8), apitossvg, width/7);
+  botaoGraves = new Botao (width/2 + (width/8), height/2 + (width/8), gravessvg, width/7);
+  setInterval(trocartext, 3000);
 
-  sair = new BotaoRedondo(100, 100, 50, color(255, 0, 0));
-  jogar = new BotaoRedondo(100, 200, 50, color(0, 255, 0));
-
-  //melodia
-  botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 100, 6, 0, PI, corMelodia, melodias);
-  //apitos
-  botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 5, 100, 6, PI, 0, corApitos, apitos);
-  //graves
-  botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 6, -PI, 0, corGraves, baixos)  ;
-  //percursão
-  botaoCirculos[4] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 6, 0, -PI, corPercursão, percussoes);
-
+  //gamestate jogar
   playButton = createButton('Play');
-  playButton.position(windowWidth / 2, windowHeight / 2);
+  playButton.position(windowWidth/2, windowHeight/2 - 300);
   playButton.mousePressed(play);
-
-  playbackSpeedSlider = createSlider(0.5, 1.5, 1, 0.1);
-  playbackSpeedSlider.position(windowWidth / 2, (windowHeight / 2 - 100));
-  playbackSpeedSlider.style('width', '80px');
-  playbackSpeedSlider.hide();
+  //painel
+  fundogeral = new painel (width/2, height/2, fundogeralsvg, width/4, width/6);
+  //SpeedSlider
+  speedSlider = createSlider(0.5, 1.5, 1, 0.1);
+  speedSlider.position(fundogeral.x, fundogeral.y);
+  speedSlider.style('width', 'fundogeral.h/9');
+  speedSlider.style('rotate', '90deg');
+  speedSlider.hide();
+  //botoes
+    //melodia
+    botaoCirculos[1] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight / 6, 100, 6, 0, PI, melodiasvg, corMelodia, melodias);
+    //apitos
+    botaoCirculos[2] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight / 5, 100, 6, PI, 0, apitossvg, corApitos, apitos);
+    //graves
+    botaoCirculos[3] = new BotaoCirculos(windowWidth - windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 6, -PI, 0, gravessvg, corGraves, baixos)  ;
+    //percursão
+    botaoCirculos[4] = new BotaoCirculos(windowWidth / 10, 0 + windowHeight - windowHeight / 6, 100, 6, 0, -PI, percussaosvg, corPercussao, percussoes);
+  
 }
 
+function draw() {
+  noStroke();
+  background(35, 53, 63);
+
+    //TIMER
+    if (frameCount % frameRate == 0) {
+      time++;
+    }
+
+  if (gamestate === "inicio") {
+    inicioButton.exibir();
+    playButton.hide();
+    speedSlider.hide();
+  } else if (gamestate === "elementos") {
+    botaoMelodia.exibir();
+    botaoPercussao.exibir();
+    botaoApitos.exibir();
+    botaoGraves.exibir();
+
+    playButton.hide();
+    speedSlider.hide();
+
+    //instruções
+    textAlign(CENTER, CENTER);
+    textSize(width / 40);
+    fill(255);
+
+    text(instrucaoElementos[indexText], width / 2, height - (height / 9));
+
+    push();
+    translate(width / 2, height / 9);
+    rotate(PI);
+    text(instrucaoElementos[indexText], 0, 0);
+    pop();
+  } else if (gamestate === "jogar") {
+    playButton.show();
+    speedSlider.show();
+    fundogeral.exibir();
+
+    for (let i = 1; i < botaoCirculos.length; i++) {
+      botaoCirculos[i].exibir();
+    }
+  }
+
+  const playbackSpeed = speedSlider.value();
+  allSounds.forEach(sound => {
+    sound.rate(playbackSpeed);
+  });
+
+}
+
+//----------TROCAR TEXTO NA PAGINA ELEMENTOS--------------------
+function trocartext(){
+  indexText++; 
+  
+  if(indexText >= instrucaoElementos.length){
+    indexText = 0; 
+  }
+}
+
+//------------- Play all sounds simultaneously with volume 0----
 function play() {
-  // Play all sounds simultaneously with volume 0
   allSounds.forEach(sound => {
     sound.play();
     sound.setVolume(0);
   });
 }
 
+//-------------FAZER O TOGGLE DO SOM-----------
 function toggleVolume(sound) {
   // Toggle the volume between 0 and 1
   sound.setVolume(sound.getVolume() === 0 ? 1 : 0);
 }
 
+//----------- CRIAR OS BOTOES PARA DAR TOGGLE-----
 function createToggleButton(label, sound, index) {
   let button = createButton(label);
 
@@ -123,129 +211,52 @@ function createToggleButton(label, sound, index) {
   button.mouseClicked(() => playApito(index));
 }
 
-function draw() {
-  noStroke();
-  background(35, 53, 63);
-
-  //TIMER
-  if (frameCount % frameRate == 0) {
-    time++;
-  }
-
-  if (gameState === "play") {
-    inicioButton.display();
-  } else if (gameState === "iniciar") {
-    melodia_button.display();
-    percursao_button.display();
-    apitos_button.display();
-    graves_button.display();
-
-    //jogar.display();
-    //sair.display();
-  } else if (gameState === "jogar") {
-    for (let i = 1; i < botaoCirculos.length; i++) {
-      botaoCirculos[i].display();
-    }
-
-    /*
-    createToggleButton('Melodia 1', melodia1, 1);
-    createToggleButton('Melodia 2', melodia2, 2);
-    createToggleButton('Melodia 3', melodia3, 3);
-    createToggleButton('Melodia 4', melodia4, 4);
-    createToggleButton('Melodia 5', melodia5, 5);
-
-    createToggleButton('Baixo 1', baixo1, 6);
-    createToggleButton('Baixo 2', baixo2, 7);
-    createToggleButton('Baixo 3', baixo3, 8);
-    createToggleButton('Baixo 4', baixo4, 9);
-    createToggleButton('Baixo 5', baixo5, 10);
-
-    createToggleButton('Percussão 1', perc1, 11);
-    createToggleButton('Percussão 2', perc2, 12);
-    createToggleButton('Percussão 3', perc3, 13);
-    createToggleButton('Percussão 4', perc4, 14);
-    createToggleButton('Percussão 5', perc5, 15);
-
-    createToggleButton('Apito 1', apito1, 16);
-    createToggleButton('Apito 2', apito2, 17);
-    createToggleButton('Apito 3', apito3, 18);
-    createToggleButton('Apito 4', apito4, 19);
-    createToggleButton('Apito 5', apito5, 20);
-*/
-    //sair.display();
-  }
-
-  if (gameState === "play" || gameState === "iniciar") {
-    playButton.hide();
-    playbackSpeedSlider.hide();
-  } else {
-    playButton.show();
-    playbackSpeedSlider.show();
-  }
-
-  const playbackSpeed = playbackSpeedSlider.value();
-  allSounds.forEach(sound => {
-    sound.rate(playbackSpeed);
-  });
-}
-
-
-
+//---------NEM SEI MUITO BEM, É PARA ESGALHAR CENAS AQUI-----
 function touchStarted() {
-
   for (let j = 0; j < touches.length; j++) {
     let x = touches[j].x;
     let y = touches[j].y;
 
-    if (gameState === "play" && inicioButton.contains(x, y)) {
-      gameState = "iniciar";
-    } else if (gameState === "iniciar" && sair.contains(x, y)) {
-      gameState = "play";
-    } else if (gameState === "iniciar") {
-      melodia_button.verificarToque(x, y);
-      percursao_button.verificarToque(x, y);
-      apitos_button.verificarToque(x, y);
-      graves_button.verificarToque(x, y);
-
-      // Verifica se todos os botões estão selecionados no iniciar
-      if (
-        melodia_button.selecionada &&
-        percursao_button.selecionada &&
-        apitos_button.selecionada &&
-        graves_button.selecionada
-      ) {
-        gameState = "jogar";
-      }
-    } else if (gameState === "jogar" && sair.contains(x, y)) {
-      gameState = "play";
-    }
-
-    for (let i = 1; i < botaoCirculos.length; i++) {
-      botaoCirculos[i].verificarToque(x, y);
+  if (gamestate === "inicio" && inicioButton.contains(x, y)) {
+    gamestate = "elementos";
+  } //experimentar codigo numa cena touch
+   else if (gamestate === "elementos" /*&& botaoMelodia.contains(x, y) && botaoGraves.contains(x, y) && botaoApitos.contains(x, y) && botaoPercussao.contains(x, y)*/) {
+    botaoMelodia.verificarToque(x, y);
+    botaoGraves.verificarToque(x, y);
+    botaoApitos.verificarToque(x, y);
+    botaoPercussao.verificarToque(x, y);
+    if (botaoMelodia.selecionada && botaoApitos.selecionada && botaoGraves.selecionada && botaoPercussao.selecionada) {
+      gamestate = "jogar";
     }
   }
+
+  for (let i = 1; i < botaoCirculos.length; i++) {
+    botaoCirculos[i].verificarToque(x, y);
+  }
+}
 }
 
 
-class BotaoRedondo {
-  constructor(x, y, diametro, cor) {
+//------CLASSES--------------
+
+//BOTOES DA GAMESTATE INICIO E ELEMENTOS
+class Botao {
+  constructor(x, y, svg, diametro) {
     this.x = x;
     this.y = y;
+    this.svg = svg;
     this.diametro = diametro;
-    this.cor = cor;
     this.selecionada = false;
   }
 
-  display() {
-    fill(this.cor);
-    noStroke();
-    ellipse(this.x, this.y, this.diametro, this.diametro);
+  exibir() {
+    image(this.svg, this.x - this.diametro / 2, this.y - this.diametro / 2, this.diametro, this.diametro);
 
-    // Adiciona um indicador visual quando o botão está selecionado
-    if (this.selecionada) {
-      fill(255, 0, 0, 100);
-      ellipse(this.x, this.y, this.diametro, this.diametro);
-    }
+        // Adiciona um indicador visual quando o botão está selecionado
+        if (this.selecionada) {
+          fill(0, 0, 0, 100);
+          ellipse(this.x, this.y, this.diametro, this.diametro);
+        }
   }
 
   contains(px, py) {
@@ -264,10 +275,25 @@ class BotaoRedondo {
   }
 }
 
+//PAINEL CENTRAL DA GAMESTATE JOGAR
+class painel {
+  constructor(x, y, svg, w, h) {
+    this.x = x;
+    this.y = y;
+    this.svg = svg;
+    this.w = w;
+    this.h = h;
+  }
 
+  exibir() {
+    image(this.svg, this.x - this.w / 2, this.y - this.h / 2, this.w, this.h);
+  }
 
+}
+
+//BOTAO PRINCIPAL ONDE CONSEGUIMOS FAZER PREVIEW DO SOM
 class BotaoCirculos {
-  constructor(x, y, diametroCentral, numCirculosSatelites, angSeletoresInicio, angSeletoresFim, cor, array) {
+  constructor(x, y, diametroCentral, numCirculosSatelites, angSeletoresInicio, angSeletoresFim, svg, cor, array) {
     this.x = x;
     this.y = y;
     this.angSeletoresInicio = angSeletoresInicio;
@@ -276,6 +302,7 @@ class BotaoCirculos {
     this.numCirculosSatelites = numCirculosSatelites;
     this.circulosSatelites = [];
     this.cor = cor;
+    this.svg = svg;
     this.array = array;
 
     for (let i = 0; i < numCirculosSatelites; i++) {
@@ -283,16 +310,16 @@ class BotaoCirculos {
       let raio = this.diametroCentral * 1.3;
       let sateliteX = this.x + raio * cos(angulo);
       let sateliteY = this.y + raio * sin(angulo);
-      this.circulosSatelites.push(new SeletorCirculos(sateliteX, sateliteY, 50, i + 1, this.cor, array));
-    }
+      this.circulosSatelites.push(new SeletorCirculos(sateliteX, sateliteY, 50, i + 1, this.cor, array));   
+     }
   }
 
-  display() {
+  exibir() {
     fill(this.cor);
-    ellipse(this.x, this.y, this.diametroCentral, this.diametroCentral);
+    image(this.svg, this.x - this.diametroCentral / 2, this.y - this.diametroCentral / 2, this.diametroCentral, this.diametroCentral);
 
     for (let i = 0; i < this.numCirculosSatelites; i++) {
-      this.circulosSatelites[i].display();
+      this.circulosSatelites[i].exibir();
     }
   }
 
@@ -305,6 +332,7 @@ class BotaoCirculos {
   }
 }
 
+// BOLAS COM OS SONS DENTRO DO BOTAO CIRCULOS
 class SeletorCirculos {
   constructor(x, y, diametro, numero, cor, array) {
     this.x = x;
@@ -320,16 +348,17 @@ class SeletorCirculos {
     this.id = 0;
   }
 
-  display() {
+exibir() {
     noStroke();
     fill(this.corAtual);
     ellipse(this.x, this.y, this.diametro, this.diametro);
-    fill(10, 43, 53);
+    fill(255);
     textAlign(CENTER, CENTER);
     textStyle(BOLD);
     textSize(24);
     text(this.numero, this.x, this.y);
   }
+  
 
   contains(px, py) {
     let d = dist(px, py, this.x, this.y);
@@ -355,3 +384,10 @@ class SeletorCirculos {
     return this.id;
   }
 }
+
+
+
+
+
+
+
